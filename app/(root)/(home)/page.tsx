80e15991695ee40169bf6b4af3777dac1e0db010
Jamshidbek Muhammadjonov <m.jamshidbek@icloud.com>
2024-07-07 18:51:37 +0500

    fix eslint errors_2

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -8,43 +8,6 @@ import { HomePageFilters } from "@/constants/filters";
 import { getQuestions } from "@/lib/actions/question.action";
 import Link from "next/link";
 
-const questions = [
-  {
-    _id: "1",
-    title: "Cascading Deletes in SQLAlchemy?",
-    tags: [
-      { _id: "1", name: "python" },
-      { _id: "2", name: "sql" },
-    ],
-    author: {
-      _id: "1",
-      name: "John Doe",
-      picture: "john-doe.jpg",
-    },
-    upvotes: 1500000,
-    views: 500552,
-    answers: [],
-    createdAt: new Date("2023-09-01T12:00:00.000Z"),
-  },
-  {
-    _id: "2",
-    title: "How to center a div?",
-    tags: [
-      { _id: "3", name: "css" },
-      { _id: "4", name: "html" },
-    ],
-    author: {
-      _id: "2",
-      name: "Jane Smith",
-      picture: "jane-smith.jpg",
-    },
-    upvotes: 5,
-    views: 50,
-    answers: [],
-    createdAt: new Date("2021-09-02T10:30:00.000Z"),
-  },
-];
-
 export default async function Home() {
   const result = await getQuestions({});
 
