d64498ec85de567c25e0e20e1d3c0a6062dc018b
Jamshidbek Muhammadjonov <m.jamshidbek@icloud.com>
2024-08-22 01:23:07 +0500

    CodeXStack

--- components/shared/navbar/MobileNav.tsx
+++ components/shared/navbar/MobileNav.tsx
@@ -78,7 +78,7 @@ const MobileNav = () => {
           />
 
           <p className="h2-bold text-dark100_light900 font-spaceGrotesk">
-            Dev <span className="text-primary-500">Overflow</span>
+            Codex<span className="text-primary-500">Stack</span>
           </p>
         </Link>
         <div>
